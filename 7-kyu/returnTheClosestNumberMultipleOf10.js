1dc033f07035dd8d5185a7a82dcb7657fac261e7
Wendy Chen <ting.wendy6@gmail.com>
2022-04-09 10:29:25 -0700

    daily practice

---
+++ 7-kyu/returnTheClosestNumberMultipleOf10.js
@@ -0,0 +1,17 @@
+Given a number return the closest number to it that is divisible by 10.
+
+Example input:
+
+22
+25
+37
+Expected output:
+
+20
+30
+40
+
+solution:
+
+const closestMultiple10 = num1 =>
+  Math.round(num1 / 10) * 10;
